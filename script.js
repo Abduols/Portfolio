d18a7a692787ebe28c4248e830cf61ea1bb73a6d
Abdulbasid Yahaya <safyanmuhammad089@gmail.com>
2025-11-21 15:34:45 +0100

    feat: added js variables and DOM in js file.

---
+++ script.js
@@ -0,0 +1,12 @@
+/** @format */
+
+// variables and DOM
+let number;
+let attempts = 0;
+const numberOfAttempts = 15;
+
+const input = document.getElementById("guess");
+const button = document.getElementById("btn");
+const feedback = document.getElementById("message");
+const attemptsDisplay = document.getElementById("attempts");
+const reset = document.getElementById("reset");
